212e2299f33915ce667fb498a14e23c3a90e42cc
Mtrizkii <m.taufikrizki@gmail.com>
2024-06-22 15:14:13 +0700

    add room page

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,10 @@
 const navigation = document.querySelector("nav .navigation");
 const menuBtn = document.querySelector(".hamBtn .menuBtn");
 menuBtn.addEventListener("click", () => {
-  navigation.classList.toggle('show')
+  navigation.classList.toggle("show");
 });
+
+// Year On Footer
+const yearCopyright = document.querySelector("footer .year");
+const dFunction = new Date();
+yearCopyright.innerHTML = dFunction.getFullYear();
