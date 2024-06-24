4f68db7857fb2a3c12abdd17c3837eb1593a53dd
uiuxkimabe <uiuxkimabe@gmail.com>
2024-06-25 06:58:36 +0700

    booking & slider

---
+++ src/js/room.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+    // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+// Slider
+const sliderPhoto = document.querySelector('#slider')
+const closeSliderPhoto = document.querySelector('#slider .close')
+const roomImg = document.querySelector('.room-img').addEventListener('click', () => {
+    sliderPhoto.classList.add('show')
+})
+
+closeSliderPhoto.addEventListener('click', () => {
+    sliderPhoto.classList.remove('show')
+})
+
+// Room Price
+const priceList = {
+    standardRoom: 699999,
+    deluxeRoom: 820000,
+    superiorRoom: 1000000
+}
+
+const priceRoom = document.querySelectorAll('.room-price .price')
+const [standard, deluxe, superior] = priceRoom
+function changePrice() {
+    standard.innerHTML = priceList.standardRoom.toLocaleString('ID-id');
+    // deluxe.innerHTML = priceList.deluxeRoom.toLocaleString('ID-id');
+    // superior.innerHTML = priceList.superiorRoom.toLocaleString('ID-id');
+}
+
+changePrice()
