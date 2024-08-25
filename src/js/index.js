1f62b8c17e6d2d3816136cac583e2cf9f177ea71
uiuxkimabe <uiuxkimabe@gmail.com>
2024-08-25 09:50:40 +0700

    making class RoomHighlight

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,34 @@ menuBtn.addEventListener("click", () => {
   navigation.classList.toggle("show");
 });
 
+// Highlight Rooms
+const rooms = document.querySelectorAll('.highlight-card .room-type')
+const roompax = document.querySelectorAll('.preview-room-content .pax')
+const pricePreview = document.querySelectorAll('.preview-room-content .price-preview')
+    
+class RoomHighlight {
+  constructor(typeRoom,pax,priceWd,priceWe) {
+    this.roomType = typeRoom;
+    this.capacity = pax;
+    this.priceWeekDay = priceWd;
+    this.priceWeekEnd = priceWe;
+  }
+  rewrite(param) {
+    rooms[param].innerHTML = `${this.roomType}`
+    roompax[param].innerHTML = `Up to ${this.capacity}`
+    pricePreview[param].innerHTML = `${this.priceWeekDay.toLocaleString("id-ID")} - ${this.priceWeekEnd.toLocaleString("id-ID")} / Night`
+  }
+}
+
+const standard = new RoomHighlight('Standard Room ', 3, 699000, 799000)
+standard.rewrite(0)
+
+const deluxe = new RoomHighlight('Deluxe Room', 3, 799000, 899000)
+standard.rewrite(1)
+
+const suites = new RoomHighlight('Suites Room', 4, 899000, 999000)
+standard.rewrite(2)
+
 // Year On Footer
 const yearCopyright = document.querySelector("footer .year");
 const dFunction = new Date();
--- src/js/room.js
+++ src/js/room.js
@@ -12,34 +12,34 @@ const swiper = new Swiper('.swiper', {
 });
 
 
-// Slider
-const sliderPhoto = document.querySelector('#slider')
-const closeSliderPhoto = document.querySelectorAll('#slider .close')
-const roomImg = document.querySelectorAll('.room-img')
+// // Slider
+// const sliderPhoto = document.querySelector('#slider')
+// const closeSliderPhoto = document.querySelectorAll('#slider .close')
+// const roomImg = document.querySelectorAll('.room-img')
 
-roomImg.forEach(element => {
-  element.addEventListener('click', () => {
-    sliderPhoto.classList.add('show')
-  })
-});
+// roomImg.forEach(element => {
+//   element.addEventListener('click', () => {
+//     sliderPhoto.classList.add('show')
+//   })
+// });
 
-closeSliderPhoto.forEach(element => {
-  element.addEventListener('click',()=> {sliderPhoto.classList.remove('show')})
-});
+// closeSliderPhoto.forEach(element => {
+//   element.addEventListener('click',()=> {sliderPhoto.classList.remove('show')})
+// });
 
-// Room Price
-const priceList = {
-    standardRoom: 699999,
-    deluxeRoom: 820000,
-    superiorRoom: 1000000
-}
+// // Room Price
+// const priceList = {
+//     standardRoom: 699999,
+//     deluxeRoom: 820000,
+//     superiorRoom: 1000000
+// }
 
-const priceRoom = document.querySelectorAll('.room-price .price')
-const [standard, deluxe, superior] = priceRoom
-function changePrice() {
-    standard.innerHTML = priceList.standardRoom.toLocaleString('ID-id');
-    deluxe.innerHTML = priceList.deluxeRoom.toLocaleString('ID-id');
-    superior.innerHTML = priceList.superiorRoom.toLocaleString('ID-id');
-}
+// const priceRoom = document.querySelectorAll('.room-price .price')
+// const [standard, deluxe, superior] = priceRoom
+// function changePrice() {
+//     standard.innerHTML = priceList.standardRoom.toLocaleString('ID-id');
+//     deluxe.innerHTML = priceList.deluxeRoom.toLocaleString('ID-id');
+//     superior.innerHTML = priceList.superiorRoom.toLocaleString('ID-id');
+// }
 
-changePrice()
+// changePrice()
